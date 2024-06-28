31981d5268ccd9d0a57326e727f00216798399f6
AlexsHomeServer <me@alex-berry.net>
2024-06-29 10:13:30 +1200

    Change home-server hardware config to include mounted drive

--- hosts/home-server/hardware-configuration.nix
+++ hosts/home-server/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
@@ -24,6 +24,16 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
+  fileSystems."/mnt/library" =
+    { device = "/dev/sdb1";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mnt/library" =
+    { device = "/dev/disk/by-uuid/627c2049-5b72-410d-baa8-4740eab7e865";
+      fsType = "ext4";
+    };
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/41c0ae5f-2476-43a3-bee5-e4d39bc21d0c"; }
     ];
@@ -33,7 +43,17 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-8aea26a4a5c7.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-c317723932ba.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-df791ede360c.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.veth-wg-br.useDHCP = lib.mkDefault true;
+  # networking.interfaces.veth44ca76d.useDHCP = lib.mkDefault true;
+  # networking.interfaces.vethb425da1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.vethfb11927.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wg-br.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f0u3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
