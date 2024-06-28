7a5697949bb32de4707dc3c75e16187907320c54
Alex Berry <me@alex-berry.net>
2024-06-29 10:22:58 +1200

    Change media library directory location

--- modules/nixos/nixarr.nix
+++ modules/nixos/nixarr.nix
@@ -8,7 +8,7 @@
     # These two values are also the default, but you can set them to whatever
     # else you want
     # WARNING: Do _not_ set them to `/home/user/whatever`, it will not work!
-    mediaDir = "/data/media";
+    mediaDir = "/mnt/library/media";
     stateDir = "/data/media/.state/nixarr";
 
     vpn = {
