5aa42080019ecf3bb11ca52bc97a5172f4a79382
Alex Berry <me@alex-berry.net>
2024-06-28 18:30:28 +1200

    Retry enabling hardware acceleration

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -82,7 +82,25 @@
     usbutils
     git
     wireguard-tools
+    jellyfin-ffmpeg
   ];
+
+  # 1. enable vaapi on OS-level
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
 
   services.openssh = {
     enable = true;
