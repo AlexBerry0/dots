6a35e7a76835348dceed3d88cdc631be8a348309
Alex Berry <me@alex-berry.net>
2024-06-28 16:25:04 +1200

    Disable hardware transcoding on server

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -71,20 +71,6 @@
     };
   };
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
-  };
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-    ];
-  };
-
   virtualisation.docker.enable = true;
 
   # Allow unfree packages
