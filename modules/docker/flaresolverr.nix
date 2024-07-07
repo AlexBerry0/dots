76b3b84ffeb6947a8d6fcc59c3370c1b50653341
Alex Berry <me@alex-berry.net>
2024-07-08 01:47:25 +1200

    Add flaresolvarr and make minecraft container not autostart

---
+++ modules/docker/flaresolverr.nix
@@ -0,0 +1,71 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  # Runtime
+  virtualisation.docker = {
+    enable = true;
+    autoPrune.enable = true;
+  };
+  virtualisation.oci-containers.backend = "docker";
+
+  # Containers
+  virtualisation.oci-containers.containers."flaresolverr" = {
+    image = "ghcr.io/flaresolverr/flaresolverr:latest";
+    environment = {
+      CAPTCHA_SOLVER = "none";
+      LOG_HTML = "false";
+      LOG_LEVEL = "info";
+      TZ = "Europe/London";
+    };
+    ports = [
+      "8191:8191/tcp"
+    ];
+    log-driver = "journald";
+    extraOptions = [
+      "--network-alias=flaresolverr"
+      "--network=flaresolverr_default"
+    ];
+  };
+  systemd.services."docker-flaresolverr" = {
+    serviceConfig = {
+      Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
+    };
+    after = [
+      "docker-network-flaresolverr_default.service"
+    ];
+    requires = [
+      "docker-network-flaresolverr_default.service"
+    ];
+    partOf = [
+      "docker-compose-flaresolverr-root.target"
+    ];
+    wantedBy = [
+      "docker-compose-flaresolverr-root.target"
+    ];
+  };
+
+  # Networks
+  systemd.services."docker-network-flaresolverr_default" = {
+    path = [pkgs.docker];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStop = "${pkgs.docker}/bin/docker network rm -f flaresolverr_default";
+    };
+    script = ''
+      docker network inspect flaresolverr_default || docker network create flaresolverr_default
+    '';
+    partOf = ["docker-compose-flaresolverr-root.target"];
+    wantedBy = ["docker-compose-flaresolverr-root.target"];
+  };
+
+  # Root service
+  systemd.targets."docker-compose-flaresolverr-root" = {
+    wantedBy = ["multi-user.target"];
+  };
+}
--- modules/docker/minecraft.nix
+++ modules/docker/minecraft.nix
@@ -15,16 +15,15 @@
     image = "itzg/minecraft-server";
     environment = {
       EULA = "true";
-      DIFFICULTY = "hard";
-      MEMORY = "6G";
     };
     volumes = [
-      "data:/data:rw"
+      "minecraft_data:/data:rw"
     ];
     ports = [
       "25565:25565/tcp"
     ];
     log-driver = "journald";
+    autoStart = false;
     extraOptions = [
       "--network-alias=mc"
       "--network=minecraft_default"
@@ -86,6 +85,5 @@
   # When started, this will automatically create all resources and start
   # the containers. When stopped, this will teardown all resources.
   systemd.targets."docker-compose-minecraft-root" = {
-    wantedBy = ["multi-user.target"];
   };
 }
