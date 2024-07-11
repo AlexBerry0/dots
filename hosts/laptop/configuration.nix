697612382fc90894c3feef22916c69dedbd1b958
Alex Berry <me@alex-berry.net>
2024-07-12 08:51:49 +1200

    Temporarily disable mullvad as the network settings it needs don't work well with captive portals

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -224,14 +224,14 @@
 
   services.mullvad-vpn.enable = true;
 
-  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = ["~."];
-    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-    dnsovertls = "true";
-  };
+  # networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+  # services.resolved = {
+  #   enable = true;
+  #   dnssec = "true";
+  #   domains = ["~."];
+  #   fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+  #   dnsovertls = "true";
+  # };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
