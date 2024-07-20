3f4662c5a8b18f335e6691269c383092df4fd1e8
Alex Berry <me@alex-berry.net>
2024-07-20 16:11:43 +1200

    Remove duplicate networking line

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -77,9 +77,6 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   # Set your time zone.
   time.timeZone = "Pacific/Auckland";
 
