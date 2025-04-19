33ec5f934aa45e5504f917955abb184e51b14e7b
Alex Berry <me@alex-berry.net>
2025-04-19 16:26:03 +1200

    Remove jetbrains plugin stuff

--- modules/personal-computers/home-manager/jetbrains.nix
+++ modules/personal-computers/home-manager/jetbrains.nix
@@ -4,18 +4,18 @@
   ...
 }: {
   home.packages = with pkgs; [
-    # jetbrains.idea-ultimate
+    jetbrains.idea-ultimate
     android-studio
-    ((jetbrains.plugins.addPlugins jetbrains.idea-ultimate [
-        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/jetbrains/plugins/plugins.json
-        "github-copilot"
-        "ide-features-trainer"
-        "nixidea"
-        "darcula-pitch-black"
-      ])
-      .overrideAttrs {
-        # copying intellij back and forth from the build server is useless and slow
-        preferLocalBuild = true;
-      })
+    # ((jetbrains.plugins.addPlugins jetbrains.idea-ultimate [
+    #     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/jetbrains/plugins/plugins.json
+    #     "github-copilot"
+    #     "ide-features-trainer"
+    #     "nixidea"
+    #     "darcula-pitch-black"
+    #   ])
+    #   .overrideAttrs {
+    #     # copying intellij back and forth from the build server is useless and slow
+    #     preferLocalBuild = true;
+    #   })
   ];
 }
